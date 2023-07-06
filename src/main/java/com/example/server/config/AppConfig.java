77ec62ce467649f98fa045ae3646b29a6a36594a
brndalx <113251192+brandalx@users.noreply.github.com>
2023-07-06 14:01:51 +0300

    feat: app config added

--- src/main/java/com/example/server/config/AppConfig.java
+++ src/main/java/com/example/server/config/AppConfig.java
@@ -1,4 +1,8 @@
 package com.example.server.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+    // Database connection configuration and other settings
 }
