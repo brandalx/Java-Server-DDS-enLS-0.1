34a179e68f5376f707dee71eaaa5678b44e276bc
brndalx <113251192+brandalx@users.noreply.github.com>
2023-07-06 14:22:36 +0300

    feat:user service

--- src/main/java/com/example/server/services/UserService.java
+++ src/main/java/com/example/server/services/UserService.java
@@ -1,4 +1,39 @@
 package com.example.server.services;
 
+import com.example.server.models.User;
+import com.example.server.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        // Logic to get all users
+    }
+
+    public User getUserById(Long id) {
+        // Logic to get a user by ID
+    }
+
+    public User createUser(User user) {
+        // Logic to create a new user
+    }
+
+    public User updateUser(Long id, User user) {
+        // Logic to update a user
+    }
+
+    public void deleteUser(Long id) {
+        // Logic to delete a user
+    }
 }
