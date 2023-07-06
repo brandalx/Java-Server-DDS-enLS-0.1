b41730466bc5f7590749ee659e8dcf37d92175da
brndalx <113251192+brandalx@users.noreply.github.com>
2023-07-06 14:19:42 +0300

    feat: javax.persistence

--- src/main/java/com/example/server/models/User.java
+++ src/main/java/com/example/server/models/User.java
@@ -1,4 +1,19 @@
 package com.example.server.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    // Getters and setters
 }
